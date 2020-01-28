76a5315c060d2211abbadc74c024b6030c38cc39
Vitor <45610681+vabreuvarela@users.noreply.github.com>
2020-01-28 21:56:20 +0000

    feature(laravel): laravel clean install

---
+++ README.md
@@ -0,0 +1,3 @@
+## ERP developed using Laravel for the api and Vue for the frontend
+
+# Currently developing the api
---
+++ webpack.mix.js
@@ -0,0 +1,18 @@
+const mix = require('laravel-mix');
+require('laravel-mix-polyfill');
+
+mix.browserSync('127.0.0.1:8000');
+
+mix.js('resources/js/app.js', 'js')
+   .sass('resources/sass/app.scss', 'css')
+   .polyfill({
+      enabled: true,
+      useBuiltIns: "usage",
+      targets: {
+        "firefox": "50",
+        "ie": 11
+      }
+   });
+
+mix.sourceMaps();
+mix.version();
